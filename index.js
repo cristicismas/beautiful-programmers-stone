428b4cbdb3646ca279c0b77b28470c4ee7bfd0d8
Cristi Cismas <cristicismas99@gmail.com>
2019-11-17 17:09:12 +0200

    Add syntax highlighting functionality

---
+++ index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const codeblocks = document.querySelectorAll("pre");
+
+  for (let codeblock of codeblocks) {
+    highlightSyntax(codeblock);
+  }
+});
+
+function highlightSyntax(element) {
+  const keyword = /\b(DWORD|WINAPI|DealerIterator|if|else|while|for|->|<|>|(|)|()|!|&|&&|<|>|void|=|==|TRUE|FALSE|break|return)(?=[^\w])/g;
+  const comment = /(\/\/.*)/g;
+
+  let parsedElement = element.textContent;
+
+  parsedElement = parsedElement
+    .replace(keyword, '<span class="keyword">$1</span>')
+    .replace(comment, '<span class="comment">$1</span>');
+
+  element.innerHTML = parsedElement;
+}
